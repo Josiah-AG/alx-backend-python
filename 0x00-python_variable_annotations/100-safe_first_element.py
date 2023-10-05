8bf18c200f3f3443da3718c5020329045882b0e1
Eyosias Gudeta <realgaz.eys@gmail.com>
2023-10-05 13:48:13 +0000

    all tasks

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""100-safe_first_element module
+"""
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """returns the first element of a list or None if the list is empty
+
+    Args:
+        lst (Sequence[Any]): list of elements
+
+    Returns:
+        Union[Any, None]: first element of a list or None if the list is empty
+    """
+    if lst:
---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""102-type_checking module
+"""
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """zoom_array function
+
+    Args:
+        lst (Tuple): list of integers
+        factor (int, optional): integer number. Defaults to 2.
+
+    Returns:
+        List: list of zoomed in
+    """
